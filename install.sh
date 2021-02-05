7832beb1c68339ea5e21e4fb388dc4fd3cbb1950
Alexander Hramov <alexander.hramov@gmail.com>
2021-02-05 15:41:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-git clone --bare https://github.com/fduch2k/dotfiles.git $HOME/.dotfiles
+git clone --bare https://github.com/fduch2k/dotfiles $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles checkout -f
